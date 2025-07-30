8e21c6301d98a3b55bb0162001e10558a0cf92b0
Vecii <vecinetflix@gmail.com>
2025-07-30 17:47:10 +0200

    "Updated app.js and fixed db.js"

--- back-end/server/config/db.js
+++ back-end/server/config/db.js
@@ -1,20 +1,19 @@
+// db.js
 const mysql = require('mysql2');
-const dotenv = require('dotenv');
-dotenv.config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+})
 
 db.connect((err) => {
   if (err) {
-    console.error('DB connection failed:', err);
-  } else {
-    console.log('Connected to MySQL database');
+    console.log('Database connection failed:', err.message);
+    return
   }
-});
+  console.log('Connected to the MySQL database.');
+})
 
 module.exports = db;
