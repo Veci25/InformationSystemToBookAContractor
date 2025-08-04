4b6accfe47fc066af7fe9aecf4cce3048d2448b4
Vecii <vecinetflix@gmail.com>
2025-08-05 00:01:57 +0200

    Fix route and controller for users

--- back-end/server/routes/users.js
+++ back-end/server/routes/users.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const { verifyToken, isAdmin } = require('../middleware/authMiddleware');
 
+router.get('/me', verifyToken, userController.getCurrentUser); 
 router.get('/', verifyToken, isAdmin, userController.getAllUsers);
 router.get('/:id', verifyToken, userController.getUserById);
-router.get('/me', verifyToken, userController.getCurrentUser); 
 router.put('/:id', verifyToken, userController.updateUser);
 router.delete('/:id', verifyToken, userController.deleteUser);
 router.post('/', verifyToken, isAdmin, userController.createUser); 
