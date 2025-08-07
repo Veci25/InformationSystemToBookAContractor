93bde4edda0ec4048933995f493c05c288879a3f
Vecii <vecinetflix@gmail.com>
2025-08-07 15:25:10 +0200

    Added register and login on the frontend

--- front-end/client/src/App.jsx
+++ front-end/client/src/App.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Register from './pages/Register';
-import Login from './pages/Login';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
 
-function App() {
+
+const App = () => {
   return (
-    <Router>
+    <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </Router>
+    </div>
   );
-}
+};
 
 export default App;
