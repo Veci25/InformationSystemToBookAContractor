277bcf526358102f6cdd8fe2a81f01e8d86da4a5
Vecii <vecinetflix@gmail.com>
2025-08-07 18:18:45 +0200

    Initial jobPost for testing

--- front-end/client/src/App.jsx
+++ front-end/client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+import JobPosts from './pages/JobPosts';
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/job-posts" element={<JobPosts />} />
       </Routes>
     </div>
   );
