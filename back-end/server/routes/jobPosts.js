e0d36208be784ce2f4a97c55fd754bee99ff65aa
Vecii <vecinetflix@gmail.com>
2025-08-12 00:16:35 +0200

    Fix jobPost router

--- back-end/server/routes/jobPosts.js
+++ back-end/server/routes/jobPosts.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
+
 const { verifyToken } = require('../middleware/authMiddleware');
-const jobPostController = require('../controllers/jobPostsController'); 
+const jobPostController = require('../controllers/jobPostsController');
 
 router.get('/', jobPostController.getAllJobPosts);
-router.get('/:id', jobPostController.getJobPostById);
-router.post('/', jobPostController.createJobPost);
-router.put('/:id', verifyToken, jobPostController.updateJobPost);
-router.delete('/:id', verifyToken,  jobPostController.deleteJobPost);
 router.get('/matches/me', verifyToken, jobPostController.getMatchesForMe);
 router.get('/:jobId/matches', verifyToken, jobPostController.getContractorsForJob);
+router.post('/', verifyToken, jobPostController.createJobPost);
+router.get('/:id', jobPostController.getJobPostById);
+router.patch('/:id', verifyToken, jobPostController.updateJobPost);
+router.put('/:id', verifyToken, jobPostController.updateJobPost);
+router.delete('/:id', verifyToken, jobPostController.deleteJobPost);
 
 module.exports = router;
